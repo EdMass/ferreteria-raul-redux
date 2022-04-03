dd3b917f59cb2516e28214b422a279a5e9766d7d
EdMass <zairmass@gmail.com>
2022-04-03 00:35:11 -0500

    autenticacion con google

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -6,25 +6,19 @@ import { useNavigate } from "react-router-dom";
 const auth = getAuth(fireApp);
 
 const Admin = () => {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
   const navigate = useNavigate();
   React.useEffect(() => {
     if (auth.currentUser) {
       console.log("existe el usuario");
-      setUser(auth.currentUser)
+      setUser(auth.currentUser);
     } else {
       console.log("no existe el usuario");
       navigate("/login");
     }
   }, [navigate]);
 
-  return <div>
-      {
-          user && (
-              <h3>{user.email}</h3>
-          )
-        }
-        </div>;
+  return <div>{user && <h3>{user.email}</h3>}</div>;
 };
 
 export default Admin;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,30 @@
 import React from "react";
-import fireApp, {db} from "../firebase/firebase";
+import fireApp, { db } from "../firebase/firebase";
 import {
   getAuth,
   createUserWithEmailAndPassword,
-  signInWithEmailAndPassword 
+  signInWithEmailAndPassword,
+  GithubAuthProvider, 
+  GoogleAuthProvider,
+  signInWithPopup
 } from "firebase/auth";
 import { collection, addDoc } from "firebase/firestore";
-import {useNavigate} from 'react-router-dom'
-
+import { useNavigate } from "react-router-dom";
 
 const auth = getAuth(fireApp);
+
+const googleAuth = new GoogleAuthProvider();
+
+const githubAuth = new GithubAuthProvider();
+
 
 const Login = () => {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [error, setError] = React.useState(null);
-  const [isRegistro, setIsRegistro] = React.useState(true)
+  const [isRegistro, setIsRegistro] = React.useState(true);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const recibirDatos = (e) => {
     e.preventDefault();
@@ -36,71 +43,75 @@ const Login = () => {
       return;
     }
 
-    setError(null)
+    setError(null);
 
-    if(isRegistro){
-      registrar()
-    }else{
-      login()
+    if (isRegistro) {
+      registrar();
+    } else {
+      login();
     }
-
   };
 
-  const login = React.useCallback(
-    async() => {
-      try {
-        const res = await signInWithEmailAndPassword(auth, email, password)
-        console.log(res)
-        setEmail('')
-        setPassword('')
-        setError(null)
-        navigate('/admin')
-      } catch (error) {
-        console.log(error);
-        if(error.message === 'Firebase: Error (auth/user-not-found).'){
-          setError('El email es incorrecto o no está registrado')
-        }
-        if(error.message === 'Firebase: Error (auth/wrong-password).'){
-          setError('El password es incorrecto o el email no está registrado')
-        }
+  const login = React.useCallback(async () => {
+    try {
+      const res = await signInWithEmailAndPassword(auth, email, password);
+      console.log(res);
+      setEmail("");
+      setPassword("");
+      setError(null);
+      navigate("/admin");
+    } catch (error) {
+      console.log(error);
+      if (error.message === "Firebase: Error (auth/user-not-found).") {
+        setError("El email es incorrecto o no está registrado");
       }
-    },
-    [email, password, navigate],
-  )
+      if (error.message === "Firebase: Error (auth/wrong-password).") {
+        setError("El password es incorrecto o el email no está registrado");
+      }
+    }
+  }, [email, password, navigate]);
 
-  const registrar = React.useCallback(
-    async() => {
-      try {
-        const res = await createUserWithEmailAndPassword(auth ,email, password)
-        await addDoc(collection(db, "usuarios"), {
-          email: res.user.email,
-          uid: res.user.uid
-        });
-        setEmail('')
-        setPassword('')
-        setError(null)
-        navigate('/admin')
-        console.log(res);
-      } catch (error) {
-        console.log(error);
-        if(error.message === 'Firebase: Error (auth/invalid-email).'){
-          setError('El email no es válido')
-        }
-        if(error.message === 'Firebase: Error (auth/email-already-in-use).'){
-          setError('El email ya está registrado')
-        }
+  const registrar = React.useCallback(async () => {
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      await addDoc(collection(db, "usuarios"), {
+        email: res.user.email,
+        uid: res.user.uid,
+      });
+      setEmail("");
+      setPassword("");
+      setError(null);
+      navigate("/admin");
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+      if (error.message === "Firebase: Error (auth/invalid-email).") {
+        setError("El email no es válido");
       }
-    },
-    [email, password, navigate],
-  )
+      if (error.message === "Firebase: Error (auth/email-already-in-use).") {
+        setError("El email ya está registrado");
+      }
+    }
+  }, [email, password, navigate]);
   
+const SignInGoogle = async() => {
+  await signInWithPopup(auth, googleAuth)
+  .then((result) => {  
+     addDoc(collection(db, "usuarios"), {
+      email: result.user.email,
+      uid: result.user.uid,
+    });
+    navigate("/admin");
+  }).catch((error) => {
+    console.log(error.code);
+    console.log(error.message);
+  });
+}
 
   return (
     <div className="mt-6">
       <h3 className="text-center">
-        {
-          isRegistro ? 'Registro de usuarios' : "Login de acceso"
-        }
+        {isRegistro ? "Registro de usuarios" : "Login de acceso"}
       </h3>
       <hr />
       <div className="row justify-content-center">
@@ -122,22 +133,36 @@ const Login = () => {
               value={password}
             />
             <div className="pagination gap-2 d-md-flex justify-content-center">
-              <button className="btn btn-dark btn-lg" type='submit'>
-                {
-                  isRegistro ? 'Registrarse' : 'Acceder'
-                }
+              <button className="btn btn-dark btn-lg" type="submit">
+                {isRegistro ? "Registrarse" : "Acceder"}
               </button>
-              <button 
-                className="btn btn-primary btn-lg" 
+              <button
+                className="btn btn-primary btn-lg"
                 onClick={() => setIsRegistro(!isRegistro)}
-                type='button'
+                type="button"
               >
-                {
-                  isRegistro ? '¿Estas registrado?' : '¿No tienes cuenta?'
-                }
+                {isRegistro ? "¿Estas registrado?" : "¿No tienes cuenta?"}
               </button>
             </div>
           </form>
+          <hr />
+          <div className="justify-content-center">
+            <button
+              className="btn btn-dark btn-sm d-md-flex mb-2"
+              style={{ margin: "0 auto" }}
+              onClick={SignInGoogle}
+            >
+              Accede con Google
+            </button>
+            
+            <button
+              className="btn btn-dark btn-sm d-md-flex"
+              style={{ margin: "0 auto" }}
+              
+            >
+              Accede con GitHub
+            </button>
+          </div>
         </div>
       </div>
     </div>
